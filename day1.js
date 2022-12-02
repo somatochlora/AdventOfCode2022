d4ca8f87ee09808a8b81039c3c93c21142b6d9cd
Reuven Martin <jaeger33@gmail.com>
2022-12-02 09:21:24 -0500

    solve part 2

--- day1.js
+++ day1.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
 let data = fs.readFileSync('data.txt', {encoding:'utf8', flag:'r'});
-let max = data.split("\r\n\r\n")
-                .map(elfStr => elfStr.split("\r\n"))
-                .map(elfArr => elfArr.reduce((prev, curr) => prev + parseInt(curr), 0))
-                .reduce((prev, curr) => Math.max(prev, curr), 0);
+
+let elfTotals = data.split("\r\n\r\n")
+                    .map(elfStr => elfStr.split("\r\n"))
+                    .map(elfArr => elfArr.reduce((prev, curr) => prev + parseInt(curr), 0));
+let max = elfTotals.reduce((prev, curr) => Math.max(prev, curr), 0);
 console.log(max);
+let topThree = elfTotals.reduce((prev, curr) => [Math.max(prev[0], curr), prev[1], prev[2]].sort(), [0, 0, 0]);
+console.log(topThree.reduce((prev, curr) => prev + curr));
