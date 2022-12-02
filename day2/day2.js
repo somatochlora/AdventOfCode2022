9c6f9d0ace66e7f2e78117b6dcb1c80cea0be284
Reuven Martin <jaeger33@gmail.com>
2022-12-02 10:29:09 -0500

    full solution

--- day2/day2.js
+++ day2/day2.js
@@ -1,19 +1,45 @@
 const fs = require('fs');
 
-let pointsMap = {
-    "A X": 4,
-    "B X": 1,
-    "C X": 7,
-    "A Y": 8,
-    "B Y": 5,
-    "C Y": 2,
-    "A Z": 3,
-    "B Z": 9,
-    "C Z": 6
+let moveMap = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "X": 1,
+    "Y": 2,
+    "Z": 3
 }
+
+let strategyMap = {
+    "X": -1,
+    "Y": 0,
+    "Z": 1
+}
+
+let calcPlayer = (outcome, opponent) => {
+    let player = opponent + outcome;
+    player = player > 3 ? 1 : player;
+    return player < 1 ? 3 : player;
+}
+
+let scoreRound = (player, opponent) => {
+    let diffMap = {
+        "-1": 0,
+        "2": 0,
+        "0": 3 ,
+        "-2": 6,
+        "1": 6
+    }
+    return diffMap[player - opponent] + player;
+};
 
 let data = fs.readFileSync('data.txt', {encoding:'utf8', flag:'r'});
 
-let rounds = data.split("\r\n");
-let score = rounds.reduce((prev, curr) => pointsMap[curr] + prev, 0);
-console.log(score);
+let rounds = data.split("\r\n").map(roundStr => [roundStr[2], roundStr[0]]);
+let naiveScore = rounds.reduce(
+    (prev, curr) => prev + scoreRound(moveMap[curr[0]],moveMap[curr[1]]),
+    0);
+console.log(naiveScore);
+let realScore = rounds.reduce(
+    (prev, curr) => prev + scoreRound(calcPlayer(strategyMap[curr[0]], moveMap[curr[1]]), moveMap[curr[1]]),
+    0);
+console.log(realScore);
