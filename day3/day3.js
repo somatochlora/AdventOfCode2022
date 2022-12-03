43e4ead6d4e80453b103998fd76c07b36db9fa2e
Reuven Martin <jaeger33@gmail.com>
2022-12-03 12:40:30 -0500

    second half solved

--- day3/day3.js
+++ day3/day3.js
@@ -6,8 +6,8 @@ let getCharValue = c => {
 
 const fs = require('fs');
 let data = fs.readFileSync('data.txt', {encoding:'utf8', flag:'r'});
-let chars = data.split("\r\n")
-                .map(row => {
+let rows = data.split("\r\n")
+let chars = rows.map(row => {
                     for(let i = 0; i < row.length / 2; i++) {
                         if (row.lastIndexOf(row[i]) >= row.length / 2) return row[i];
                     }
@@ -15,3 +15,15 @@ let chars = data.split("\r\n")
 let output = chars.map(char => getCharValue(char))
                 .reduce((prev, curr) => prev + curr, 0);
 console.log(output)
+
+let groups = [];
+for (let i = 2; i < rows.length; i += 3) {
+    groups.push([rows[i], rows[i - 1], rows[i -2]]);
+}
+output = groups.map(group => {
+                for (let char of group[0]) {
+                    if (group[1].indexOf(char) >= 0 && group[2].indexOf(char) >= 0) return char;
+                }
+            }).reduce((prev, curr) => prev + getCharValue(curr), 0);
+
+console.log(output);
