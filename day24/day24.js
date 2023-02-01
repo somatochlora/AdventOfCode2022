b6166be0c152d0fe6f92bb05f6ab943669c0d1b8
Reuven Martin <jaeger33@gmail.com>
2023-02-01 16:55:59 -0500

    part 2 solution

--- day24/day24.js
+++ day24/day24.js
@@ -86,7 +86,77 @@ while(!possibleMap[height - 1][width - 1]) {
 
 console.log(step);
 
+possibleMap = generateMap();
 
+while(!possibleMap[0][0]) {
+    step++;
+    let blizzardMap = generateMap();
+    blizzards.forEach(blizzard => {
+        blizzard.pos[0] += blizzard.dir[0];
+        if (blizzard.pos[0] < 0) blizzard.pos[0] = height - 1;
+        if (blizzard.pos[0] >= height) blizzard.pos[0] = 0;
+        blizzard.pos[1] += blizzard.dir[1];
+        if (blizzard.pos[1] < 0) blizzard.pos[1] = width - 1;
+        if (blizzard.pos[1] >= width) blizzard.pos[1] = 0;
+        blizzardMap[blizzard.pos[0]][blizzard.pos[1]] = true;
+    });
 
+    let newPosMap = generateMap();
+    if(!blizzardMap[height - 1][width - 1]) newPosMap[height - 1][width - 1] = true;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            if (possibleMap[row][col]) {
+                directions.forEach(dir => {
+                    if (row + dir[0] >= 0 &&
+                        row + dir[0] < height &&
+                        col + dir[1] >= 0 &&
+                        col + dir[1] < width &&
+                        !blizzardMap[row + dir[0]][col + dir[1]]) 
+                    {
+                        newPosMap[row + dir[0]][col + dir[1]] = true;
+                    }
+                });
+            }
+        }
+    }
+    possibleMap = newPosMap;
+}
 
+console.log(step);
 
+possibleMap = generateMap();
+
+while(!possibleMap[height - 1][width - 1]) {
+    step++;
+    let blizzardMap = generateMap();
+    blizzards.forEach(blizzard => {
+        blizzard.pos[0] += blizzard.dir[0];
+        if (blizzard.pos[0] < 0) blizzard.pos[0] = height - 1;
+        if (blizzard.pos[0] >= height) blizzard.pos[0] = 0;
+        blizzard.pos[1] += blizzard.dir[1];
+        if (blizzard.pos[1] < 0) blizzard.pos[1] = width - 1;
+        if (blizzard.pos[1] >= width) blizzard.pos[1] = 0;
+        blizzardMap[blizzard.pos[0]][blizzard.pos[1]] = true;
+    });
+
+    let newPosMap = generateMap();
+    if(!blizzardMap[0][0]) newPosMap[0][0] = true;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            if (possibleMap[row][col]) {
+                directions.forEach(dir => {
+                    if (row + dir[0] >= 0 &&
+                        row + dir[0] < height &&
+                        col + dir[1] >= 0 &&
+                        col + dir[1] < width &&
+                        !blizzardMap[row + dir[0]][col + dir[1]]) 
+                    {
+                        newPosMap[row + dir[0]][col + dir[1]] = true;
+                    }
+                });
+            }
+        }
+    }
+    possibleMap = newPosMap;
+}
+console.log(step);
