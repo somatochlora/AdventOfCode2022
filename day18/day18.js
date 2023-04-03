b999b586fc3412a89d7a42292ad1cc4027d9b9e4
Reuven Martin <jaeger33@gmail.com>
2023-04-03 10:10:42 -0400

    day 18 part 2 solved

--- day18/day18.js
+++ day18/day18.js
@@ -29,6 +29,12 @@ let countEmptyAround = (cubeMap, cube, faces) => {
     }, 0);
 }
 
+let countExposedAround = (cubeMap, outsideMap, cube, faces) => {
+    return faces.reduce((total, face) => {
+        return total + (checkFaceExposed(cubeMap, outsideMap, cube, face) ? 1 : 0);
+    }, 0);
+}
+
 let faces = [[0, 0, 1], [0, 0, -1], [0, 1, 0], [0, -1, 0], [1, 0, 0], [-1, 0, 0]];
 
 let checkFace = (cubeMap, cube, face) => {
@@ -36,12 +42,63 @@ let checkFace = (cubeMap, cube, face) => {
     return checkLoc(cubeMap, calcHash(newPos.x, newPos.y, newPos.z));
 }
 
+let checkFaceExposed = (cubeMap, outsideMap, cube, face) => {
+    let newPos = addFace(cube, face);
+    let newHash = calcHash(newPos.x, newPos.y, newPos.z);
+    return outsideMap.has(newHash);
+}
+
 let cubes = lines.map(line => lineToCube(line));
 console.log(cubes);
 let cubeMap = new Map();
 cubes.forEach(cube => cubeMap.set(cube.hash, cube));
 
-let outside = cubes.reduce((total, cube) => {
+let air = cubes.reduce((total, cube) => {
     return total + countEmptyAround(cubeMap, cube, faces);
+}, 0);
+console.log(air);
+
+let min = cubes.reduce((min, cube) => {
+    return Math.min(min, cube.x, cube.y, cube.z);
+}, 0);
+let max = cubes.reduce((max, cube) => {
+    return Math.max(max, cube.x, cube.y, cube.z);
+}, 0);
+max += 1;
+min -= 1;
+
+let expandSteam = (cubeMap, outsideMap, min, max, coords, targetList) => {
+    let hash = calcHash(coords.x, coords.y, coords.z);
+    if (outsideMap.has(hash)) {
+        return;
+    }
+    outsideMap.add(hash);
+    faces.forEach(face => {
+        let newPos = addFace(coords, face);
+        let newHash = calcHash(newPos.x, newPos.y, newPos.z);
+        if (!cubeMap.has(newHash) && checkIfInBounds(min, max, newPos.x, newPos.y, newPos.z)) {
+            targetList.add(newPos);
+        }
+    });
+}
+
+let expandSteamWrapper = (cubeMap, outsideMap, min, max, coords) => {
+    let targetList = new Set();
+    targetList.add(coords)
+    targetList.forEach(coords => {
+        expandSteam(cubeMap, outsideMap, min, max, coords, targetList);
+    });
+}
+
+let checkIfInBounds = (min, max, x, y, z) => {
+    return x >= min && x <= max && y >= min && y <= max && z >= min && z <= max;
+};
+
+let outsideMap = new Set()
+
+expandSteamWrapper(cubeMap, outsideMap, min, max, {x: -1, y: -1, z: -1});
+
+let outside = cubes.reduce((total, cube) => {
+    return total + countExposedAround(cubeMap, outsideMap, cube, faces);
 }, 0);
 console.log(outside);
